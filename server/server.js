707452bcda89484c39d4038ad47a84ad5c971ea6
Adepu Chaitanya <149540367+itachi200218@users.noreply.github.com>
2025-04-29 15:20:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -65,8 +65,7 @@ app.use('/api/messages', messageRoutes);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ 
 })
 .then(() => {
   console.log('✅ Database connected');
