bd54161f913402e8b202258f5a30b8b0d868988a
Adepu Chaitanya <149540367+itachi200218@users.noreply.github.com>
2025-08-27 04:41:33 +0530

    Delete client/src/utils/config.js

--- client/src/utils/config.js
+++
@@ -1,4 +0,0 @@
-export const API_BASE_URL = 
-  process.env.NODE_ENV === 'development' 
-    ? 'http://localhost:5000'  // For local development
-    : 'https://social-media-app-sd36.onrender.com';  // For production
